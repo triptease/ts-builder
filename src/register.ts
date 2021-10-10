8bd9ce65412e66d0accefda79e94dd43bd9af140
Theodore Messinezis <theomessin@users.noreply.github.com>
2021-10-10 02:42:20 +0100

    fix: getOutputFileNames failing

--- src/register.ts
+++ src/register.ts
@@ -8,12 +8,12 @@ const retrieveFile = (path: string): string => {
     if (extname(path) !== ".ts") return readFileSync(path, "utf-8");
 
     const tsconfigPath = ts.findConfigFile(path, ts.sys.fileExists);
-    const tsconfig = ts.readConfigFile(tsconfigPath, ts.sys.readFile).config;
-    const parsedTsconfig = ts.parseJsonConfigFileContent(tsconfig, ts.sys, dirname(tsconfigPath));
     const solutionBuilderHost = ts.createSolutionBuilderHost();
     const solutionBuilder = ts.createSolutionBuilder(solutionBuilderHost, [tsconfigPath], {});
     solutionBuilder.build(tsconfigPath);
 
+    const parseConfigFileHost =  ts.sys as unknown as ts.ParseConfigFileHost
+    const parsedTsconfig = ts.getParsedCommandLineOfConfigFile(tsconfigPath, {}, parseConfigFileHost);
     const compiledFilePath = ts.getOutputFileNames(parsedTsconfig, path, false)[0];
     return readFileSync(compiledFilePath, "utf-8");
 }

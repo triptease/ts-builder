b7573057fdd7de99f406c9e26f7d0dd86fbfd253
Theodore Messinezis <theomessin@users.noreply.github.com>
2021-11-02 18:43:43 +0200

    revert: tsx files are no longer supported

--- src/register.ts
+++ src/register.ts
@@ -1,37 +1,24 @@
 import * as ts from "typescript";
-import {dirname, extname, join} from "path";
-import {existsSync, readFileSync, unlinkSync, writeFileSync} from "fs";
+import {dirname, extname} from "path";
+import {existsSync, readFileSync} from "fs";
 import {install as sourceMap} from "source-map-support";
 
 const retrieveFile = (path: string): string => {
     if (existsSync(path) === false) return;
-    if (extname(path) !== ".ts" && extname(path) !== ".tsx") return readFileSync(path, "utf-8");
+    if (extname(path) !== ".ts") return readFileSync(path, "utf-8");
 
     const tsconfigPath = ts.findConfigFile(path, ts.sys.fileExists);
+    const solutionBuilderHost = ts.createSolutionBuilderHost();
+    const solutionBuilder = ts.createSolutionBuilder(solutionBuilderHost, [tsconfigPath], {});
+    solutionBuilder.build(tsconfigPath);
+
     const parseConfigFileHost =  ts.sys as unknown as ts.ParseConfigFileHost
     const parsedTsconfig = ts.getParsedCommandLineOfConfigFile(tsconfigPath, {}, parseConfigFileHost);
-
-    const tsBuilderConfig = {...parsedTsconfig.raw, compilerOptions: {
-        ...parsedTsconfig.raw.compilerOptions, 
-        jsx: "react",
-        tsBuildInfoFile: "tsconfig.tsbuildinfo",
-    }};
-
-    const tsBuilderConfigPath = join(dirname(tsconfigPath), "tsconfig.tsbuilder.json");
-    writeFileSync(tsBuilderConfigPath, JSON.stringify(tsBuilderConfig));
-
-    const solutionBuilderHost = ts.createSolutionBuilderHost();
-    const solutionBuilder = ts.createSolutionBuilder(solutionBuilderHost, [tsBuilderConfigPath], {});
-    solutionBuilder.build(tsBuilderConfigPath);
-    unlinkSync(tsBuilderConfigPath);
-
-    // TODO: parse the tsBuilderConfig instead of the original one.
-    let compiledFilePath = ts.getOutputFileNames(parsedTsconfig, path, false)[0];
-    if (extname(compiledFilePath) === ".jsx") compiledFilePath = compiledFilePath.slice(0, -1);
+    const compiledFilePath = ts.getOutputFileNames(parsedTsconfig, path, false)[0];
     return readFileSync(compiledFilePath, "utf-8");
 }
 
-require.extensions[".ts"] = require.extensions[".tsx"] = (module: NodeJS.Module, filename) =>
+require.extensions[".ts"] = (module: NodeJS.Module, filename) =>
     // @ts-ignore
     module._compile(retrieveFile(filename), filename);
 

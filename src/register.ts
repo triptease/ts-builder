ca1544c71e09f18044364f075e5f8ccf8a9d0ea5
Theodore Messinezis <theomessin@users.noreply.github.com>
2021-11-02 19:18:58 +0200

    feat: purge output files from deleted source files
    Co-authored-by: Tom Damant <32488109+tdamant@users.noreply.github.com>

--- src/register.ts
+++ src/register.ts
@@ -1,13 +1,31 @@
 import * as ts from "typescript";
-import {dirname, extname} from "path";
-import {existsSync, readFileSync} from "fs";
+import {dirname, extname, join} from "path";
 import {install as sourceMap} from "source-map-support";
+import {existsSync, readFileSync, unlinkSync, writeFileSync} from "fs";
+
+const purgeStaleOutputFiles = (tsbuildinfoPath: string) => {
+    const tsbuildinfo = JSON.parse(readFileSync(tsbuildinfoPath, "utf8"));
+    const fileNames: string[] = tsbuildinfo.program.fileNames;
+    const fileNamesToPurge = fileNames.filter(f => !f.endsWith(".d.ts") && extname(f) === ".ts" && !existsSync(f));
+    fileNamesToPurge.forEach(f => {
+        [".js", ".d.ts", ".js.map"].forEach(ext => {
+            const path = f.replace(".ts", ext);
+            if (existsSync(path)) unlinkSync(path);
+        });
+    });
+
+    tsbuildinfo.program.fileNames = tsbuildinfo.program.fileNames.filter((f: string) => !fileNamesToPurge.includes(f));
+    writeFileSync(tsbuildinfoPath, JSON.stringify(tsbuildinfo), "utf8");
+}
 
 const retrieveFile = (path: string): string => {
     if (existsSync(path) === false) return;
     if (extname(path) !== ".ts") return readFileSync(path, "utf-8");
 
     const tsconfigPath = ts.findConfigFile(path, ts.sys.fileExists);
+    const tsbuildinfoPath = join(dirname(tsconfigPath), "tsconfig.tsbuildinfo");
+    if (existsSync(tsbuildinfoPath)) purgeStaleOutputFiles(tsbuildinfoPath);
+
     const solutionBuilderHost = ts.createSolutionBuilderHost();
     const solutionBuilder = ts.createSolutionBuilder(solutionBuilderHost, [tsconfigPath], {});
     solutionBuilder.build(tsconfigPath);
